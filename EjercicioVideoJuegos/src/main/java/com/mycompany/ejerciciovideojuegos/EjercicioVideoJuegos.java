ffdd6e3d9d7f877cfa4955a72c10e6b190491765
Galbfran <francogalbiati984@gmail.com>
2023-10-06 16:46:29 -0300

    subo ejercicio tienda videojuegos

---
+++ EjercicioVideoJuegos/src/main/java/com/mycompany/ejerciciovideojuegos/EjercicioVideoJuegos.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciovideojuegos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author HP
+ */
+public class EjercicioVideoJuegos {
+
+    public static void main(String[] args) {
+        List <Videojuegos> catalogo = new ArrayList<Videojuegos>();
+        
+        catalogo.add(new Videojuegos(0, "Dragon ball", "Play", 2, "Pelea"));
+        catalogo.add(new Videojuegos(1, "football", "Play", 4, "Football"));
+        catalogo.add(new Videojuegos(2, "Tenis", "Pc", 2, "Tenis"));
+        catalogo.add(new Videojuegos(3, "Rapidos y Furiosos", "Xbox", 2, "Carreras"));
+        
+        for (Videojuegos juego : catalogo) {
+            juego.getCaracteristicas();
+        }
+        System.out.println("---------------------------------------------");
+        catalogo.get(1 ).setCategoria("Deportes");
+        catalogo.get(2).setCategoria("Depostes");
+        //mostrar modidificaiones
+          for (Videojuegos juego : catalogo) {
+            juego.getCaracteristicas();
+        }
+        // recorrer y mostrar solo los objetos play
+        System.out.println("---------------------------------------------");
+        
+            for (Videojuegos juego : catalogo) {
+            if ("Play".equals(juego.getConsola())){
+                juego.getCaracteristicas();
+            }
+        }
+          
+        
+    }
+}
---
+++ EjercicioVideoJuegos/src/main/java/com/mycompany/ejerciciovideojuegos/Videojuegos.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejerciciovideojuegos;
+
+/**
+ *
+ * @author HP
+ */
+public class Videojuegos {
+    int codigo;
+    String titulo;
+    String consola;
+    int CantidadJugadores; 
+    String categoria;
+    
+    public Videojuegos(){
+    }
+
+    public Videojuegos(int codigo, String titulo, String consola, int CantidadJugadores, String categoria) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.consola = consola;
+        this.CantidadJugadores = CantidadJugadores;
+        this.categoria = categoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getConsola() {
+        return consola;
+    }
+
+    public void setConsola(String consola) {
+        this.consola = consola;
+    }
+
+    public int getCantidadJugadores() {
+        return CantidadJugadores;
+    }
+
+    public void setCantidadJugadores(int CantidadJugadores) {
+        this.CantidadJugadores = CantidadJugadores;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    public void getCaracteristicas(){
+        System.out.println("El juego es "+ getTitulo() + ". Para la consola: " + getConsola() + " . La cantidad de jugadores es:" + getCantidadJugadores() + " .Categoria: " + getCategoria() );
+    }
+    
+    
+}
