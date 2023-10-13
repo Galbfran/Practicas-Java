025d740081770b7639ca53d27380fc1e5441ebd5
Galbfran <francogalbiati984@gmail.com>
2023-10-13 12:06:54 -0300

    configurando con base de datos

---
+++ practicaDos/src/main/java/com/example/practicaDos/service/PersonaService.java
@@ -0,0 +1,10 @@
+package com.example.practicaDos.service;
+
+import com.example.practicaDos.repository.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PersonaService {
+    @Autowired
+    private PersonaRepository persoRepository;
+
+}
